4b8ea25ca425fb26611a21aea2a05bca4cc72fa3
chengpan <chengpan@lejingda.com>
2018-04-20 15:07:15 +0800

    初次提交

---
+++ app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author master
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
